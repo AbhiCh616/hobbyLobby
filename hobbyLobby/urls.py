aa47de6c8a674d1f4a7d114937e2fada05a06eaf
AbhiCh616 <59159601+AbhiCh616@users.noreply.github.com>
2020-06-18 16:53:04 +0530

    Integration of Django and React
    Completed

--- hobbyLobby/urls.py
+++ hobbyLobby/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    path('', include('login.urls')),
+    path('signup/', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
 ]
