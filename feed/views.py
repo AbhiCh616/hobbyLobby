ec31a0ae8e17be48f37f750d9b4dbeeda22a4444
AbhiCh616 <59159601+AbhiCh616@users.noreply.github.com>
2020-07-22 16:06:25 +0530

    No major change

--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,5 @@
-from feed.serializers import MusicSerializer,DanceSerializer,SkatingSerializer
-from feed.models import Music,Dance,Skating
+from feed.serializers import MusicSerializer, DanceSerializer, SkatingSerializer
+from feed.models import Music, Dance, Skating
 from django.http import HttpResponseRedirect
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
@@ -13,22 +13,32 @@ def musicList(request):
     musics = Music.objects.all()
     serializer = MusicSerializer(musics, many=True)
     return Response(serializer.data)
+
+
 @api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
 def danceList(request):
     dances = Dance.objects.all()
-    serializer =DanceSerializer(dances, many=True)
+    serializer = DanceSerializer(dances, many=True)
     return Response(serializer.data)
+
+
 @api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
 def skatingList(request):
     skatings = Skating.objects.all()
     serializer = SkatingSerializer(skatings, many=True)
     return Response(serializer.data)
 
+
 @api_view(['POST'])
+@authentication_classes([])
+@permission_classes([])
 def MusicCreate(request):
-    serializer=MusicSerializer(data=request.data)
+    serializer = MusicSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return True
     return False
-
