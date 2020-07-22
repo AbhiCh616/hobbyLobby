5d391e18b6b63ffa828075976c5e9dd13be3d700
AbhiCh616 <59159601+AbhiCh616@users.noreply.github.com>
2020-07-22 16:06:58 +0530

    No major change

--- feed/serializers.py
+++ feed/serializers.py
@@ -1,15 +1,19 @@
 from rest_framework import serializers
-from feed.models import Music,Dance,Skating
+from feed.models import Music, Dance, Skating
 
 
 class MusicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Music
         fields = '__all__'
+
+
 class DanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dance
         fields = '__all__'
+
+
 class SkatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skating
--- feed/urls.py
+++ feed/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from feed.views import musicList,danceList,skatingList,MusicCreate
+from feed.views import musicList, danceList, skatingList, MusicCreate
 
 
 urlpatterns = [
     path('music_list/', musicList),
     path('dance_list/', danceList),
     path('skating_list/', skatingList),
-    path('music_list/create',MusicCreate),
+    path('music_list/create', MusicCreate),
 ]
